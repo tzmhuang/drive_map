b4454ca7a109818c61240bd48d338c212dc1ca7e
tzm <tzm@email.com>
2023-09-27 21:46:58 -0400

    debug

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('./map_webapp/main.html')
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
