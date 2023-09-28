ec048a539ca5ee5b7e5056857327302141bde8e4
tzm <tzm@email.com>
2023-09-28 01:49:33 -0400

    debug

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__, template_folder='map_webapp')
 
+
+@app.route('/upload', methods=["GET", "POST"])
+def file_upload():
+    print('HERERRERERR')
+    return render_template("./upload.html")
+
 @app.route('/')
 def hello():
-    print('HERE')
     print(os.listdir())
     return render_template('./main.html')
 
