d1f2222cef1cc9e1b9550be57db03729647443e7
Zanming Huang <37981836+tzmhuang@users.noreply.github.com>
2024-02-27 21:21:07 -0500

    Update geo_points.js
    Test fetch

--- static/geo_points.js
+++ static/geo_points.js
@@ -11,13 +11,40 @@
 // import pts_upload from './upload_data.json';// assert { type: 'json' };
 // console.log(pts_upload)
 
+// -----------
+
+// const pts_nusc = require('./nusc_data.json');
+// const pts_kitti = require('./kitti_data.json');
+
+// const pts_custom = require('./custom_data.json');
+// const pts_web = require('./youtube_data.json');
+// const pts_upload = require('./upload_data.json');
+
+// export { pts_nusc, pts_kitti, pts_web, pts_custom, pts_upload };
+
+// -----------
 
 
-const pts_nusc = require('./nusc_data.json');
-const pts_kitti = require('./kitti_data.json');
+async function loadNames() {
+  const nusc_response = await fetch('./nusc_data.json');
+  const pts_nuss = await response.json();
 
-const pts_custom = require('./custom_data.json');
-const pts_web = require('./youtube_data.json');
-const pts_upload = require('./upload_data.json');
+  const kitti_response = await fetch('./kitti_data.json');
+  const pts_kitti = await response.json();
 
-export { pts_nusc, pts_kitti, pts_web, pts_custom, pts_upload };
+  const custom_response = await fetch('./custom_data.json');
+  const pts_custom = await response.json();
+
+  const web_response = await fetch('./youtube_data.json');
+  const pts_web = await response.json();
+
+  const upload_response = await fetch('./upload_data.json');
+  const pts_upload = await response.json();
+
+  export { pts_nusc, pts_kitti, pts_web, pts_custom, pts_upload };
+  
+  // console.log(names); 
+  // logs [{ name: 'Joker'}, { name: 'Batman' }]
+}
+
+loadNames();
